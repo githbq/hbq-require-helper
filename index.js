d9bc45551d718a306a8d347a6c06bb23e27a5c3a
Alexandre Richonnier <heralight@users.noreply.github.com>
2017-06-21 11:59:38 +0200

    Update index.js to english

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-//自动加载对应目录的文件内容
-const requireDir = require('require-dir'); //require-dir 会过滤掉自身 所以不用担心会引发递归调用问题 比如 requireDir('./')
+// Helper to automatically load the contents of the corresponding directory
+
+// require-dir will filter out itself so do not worry about recursive call problems such as requireDir ('./')
+const requireDir = require('require-dir'); 
 const pathTool = require('path');
 const ioHelper = require('io-helper').default;
 const _ = require('lodash')
 module.exports = {
-    //普通require 支持数组的参数
+    // ordinary require support array parameters
     require(paths) {
         paths = [].concat(paths);
         let pathValue = pathTool.join.apply(null, paths);
@@ -14,50 +16,54 @@ module.exports = {
         }
         return result
     },
-    /**
-     * 从根节点引用模板
-     * @param {*路径　相对于程序运行的根节点} paths 
-     */
+    / **
+     * Reference the template from the root node
+     * @param {* path relative to the root of the program running} paths
+     * /
     requireRoot(paths) {
         return this.require.call(null, [process.cwd()].concat(paths));
     },
-    //按规则过滤路径require
+     / **
+     * Filter the path by rule require
+     * @param {* path relative to the root of the program running} paths
+     * /
     requireRuled(paths, cb, filterCb) {
         paths = [].concat(paths);
         let pathValue = pathTool.join.apply(null, paths);
         let dirData = requireDir(pathValue);
-        //路径过滤
+        // path filter
         _.each(dirData, (result, name) => {
-            if ((filterCb && filterCb(name)) || (!filterCb && this.nameRule(name))) { //非入口JS 并且文件名非下划线开头
+            if ((filterCb && filterCb(name)) || (!filterCb && this.nameRule(name))) { 
+                // non-entry JS and file names are underlined
                 cb && cb(result && result.default ? result.default : result, name);
             }
         });
     },
-    /**
-     * 
-     * @param {路径,一个字符串,或者一个字符串数组,会自动join拼接} paths 
-     * @param {如果定义并且返回false,会跳过本次结果,如果定义并返回新结果,则用新结果} cb 
-     * 返回路径文件结果的数组   [{result,name}]
-     */
+    / **
+     *
+     * @param {path, a string, or a string array, will automatically join splicing} paths
+     * @param {if defined and returns false, will skip this result, if the definition and return the new results, then use the new results} cb
+     * Returns the array of path file results [{result, name}]
+     * /
     requireDir(paths, cb, filterCb) {
         let resultData = [];
-        //路径过滤
+        // path filter
         this.requireRuled(paths, (result, name) => {
             const newResult = cb && cb(result, name);
             if (cb && newResult) {
                 resultData.push({ result: newResult, name });
             } else if (cb && newResult === false) {
-                //定义了回调,但返回了false 则跳过本轮
+                // Defines the callback, but returns false to skip the current round
             } else {
                 resultData.push({ result, name });
             }
         }, filterCb || this.nameRule);
         return resultData;
     },
-    //返回路径文件结果的对象   {[name]:result}]
+    // Returns the object of the path file result {[name]: result}]
     requireDirKV(paths, cb) {
         let resultData = {};
-        //路径过滤
+        // path filter
         this.requireRuled(paths, (result, name) => {
             resultData[name] = result;
             cb && cb(result, name);
@@ -67,18 +73,18 @@ module.exports = {
     nameRule(name) {
         return !(/^(index|_)/.test(name));
     },
-    //递归查找文件
+    // Recursively find the file
     requireRecurse(path, filter, cb, filterCb) {
         let dirInfos = ioHelper.findRecurseSync(path, filter);
         let dirData = { arr: [], kv: {} };
         dirInfos.forEach((n) => {
-            // if (n.isFile) {  //注释后 文件夹也要了
-            //取得文件的相对路径名称且含文件名    xxx\\yyy.js=> xxx
+            // if (n.isFile) {// After the note has the folder
+            // get the relative pathname of the file and include the file name xxx \\ yyy.js => xxx
             let dirname = pathTool.dirname(n.relative);
             let extName = pathTool.extname(n.relative);
-            //取得 yyy
+             // get yyy
             let name = pathTool.basename(n.relative, extName);
-            let fullname = ioHelper.replaceSep(pathTool.join(dirname, name)); //  相对路径  aaa/xxx/yyy.js=>  aaa/xxx/yyy 
+            let fullname = ioHelper.replaceSep(pathTool.join(dirname, name)); // relative path aaa / xxx / yyy.js => aaa / xxx / yyy 
             if ((filterCb && filterCb(name, dirname, n)) || (!filterCb && this.nameRule(name))) {
                 let result = this.require(n.filePath);
                 cb && cb(fullname, result, n);
